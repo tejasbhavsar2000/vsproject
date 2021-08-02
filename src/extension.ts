b4431480fe84554b630fdd3cf0128d1b9d71ae20
unknown <tbhavsar121@gmail.com>
2021-08-02 22:31:09 +0530

    added sidebar

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,30 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { DepNodeProvider } from './nodeDependencies';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+	const rootPath = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0))
+		? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
+	console.log(rootPath);
 	console.log('Congratulations, your extension "vsproject" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vsproject.helloWorld', () => {
+
+  context.subscriptions.push(
+    vscode.window.registerTreeDataProvider("vsproject-sidebar", new DepNodeProvider(rootPath))
+  );
+	context.subscriptions.push(vscode.commands.registerCommand('vsproject.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from VSproject!');
-	});
-
-	context.subscriptions.push(disposable);
+	}));
 }
 
 // this method is called when your extension is deactivated
---
+++ src/nodeDependencies.ts
@@ -0,0 +1,58 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+export class DepNodeProvider implements vscode.TreeDataProvider<Dependency> {
+
+    private _onDidChangeTreeData: vscode.EventEmitter<Dependency | undefined | void> = new vscode.EventEmitter<Dependency | undefined | void>();
+    readonly onDidChangeTreeData: vscode.Event<Dependency | undefined | void> = this._onDidChangeTreeData.event;
+
+    constructor(private workspaceRoot: string | undefined) {
+    }
+
+    refresh(): void {
+        this._onDidChangeTreeData.fire();
+    }
+
+    getTreeItem(element: Dependency): vscode.TreeItem {
+        return element;
+    }
+
+    getChildren(element?: Dependency): Thenable<Dependency[]> {
+        if (!this.workspaceRoot) {
+            vscode.window.showInformationMessage('No dependency in empty workspace');
+            return Promise.resolve([]);
+        }
+        vscode.window.showInformationMessage('it has something');
+        return Promise.resolve([]);
+
+    }
+
+    /**
+     * Given the path to package.json, read all its dependencies and devDependencies.
+     */
+	
+
+}
+
+export class Dependency extends vscode.TreeItem {
+
+	constructor(
+		public readonly label: string,
+		private readonly version: string,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+		public readonly command?: vscode.Command
+	) {
+		super(label, collapsibleState);
+
+		this.tooltip = `${this.label}-${this.version}`;
+		this.description = this.version;
+	}
+
+	iconPath = {
+		light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
+		dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
+	};
+
+	contextValue = 'dependency';
+}
